d20bbbff9c539ca2cf12d263b9f6918aef0decb5
Samy Farahat <Samyfarahat2003@gmail.com>
2020-11-02 14:49:57 +0100

    taak08 - Document Object Model

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -6,4 +6,10 @@ console.log(window.document);
 // document.querySelector("#posterimg1").src = null;
 
 
-// document.querySelector("#poster1");
+let poster1 = document.querySelector("#poster1");
+let poster2 = document.querySelector("#poster2");
+let poster3 = document.querySelector("#poster3");
+
+poster1.style.backgroundColor = 'red';
+poster2.style.backgroundColor = 'white';
+poster3.style.backgroundColor = 'darkblue';
