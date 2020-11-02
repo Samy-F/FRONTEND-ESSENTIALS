c79efabe87634d8d11ff9d68efe6abab15d88f93
Samy Farahat <Samyfarahat2003@gmail.com>
2020-11-02 14:18:23 +0100

    taak02 - Datatypes

--- 05 - Javascript/taak01 - Console.log/script.js
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -1,1 +1,10 @@
 console.log('mijn naam is Samy');
+
+let voornaam = 'Samy';
+let achternaam = 'Farahat';
+let leeftijd = 17;
+let ikBenStemGerechtig = false;
+let ikMagLegaalAlcoholKopen = false;
+let ikMagBrommerRijden = false;
+let SoftwareOntwikkeling = true;
+
